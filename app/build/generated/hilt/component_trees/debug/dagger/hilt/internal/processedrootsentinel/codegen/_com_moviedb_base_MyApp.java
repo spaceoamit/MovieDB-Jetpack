7cdfc4e0bf3e4fe95cb5b45a50cd8f4cc58ed993
Amit Patoliya <amit_patoliya@spaceotechnologies.com>
2022-09-14 11:55:32 +0530

    remove unused code

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_moviedb_base_MyApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.moviedb.base.MyApp"
-)
-public final class _com_moviedb_base_MyApp {
-}
